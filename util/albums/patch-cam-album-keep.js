a67f6edd40ebbb082af7e7063caa0ef0d9e7db75
Han Lin <hotdogee@gmail.com>
2019-08-13 19:35:44 +0800

    fix patch cam

--- util/albums/patch-cam-album-keep.js
+++ util/albums/patch-cam-album-keep.js
@@ -18,21 +18,22 @@ socket.on('connect', async (connection) => {
   try {
     // eslint-disable-next-line no-unused-vars
     const auth = await api.reAuthenticate()
-    await Promise.all(
-      albumNames.map(async (name) => {
-        const {
-          data: [album]
-        } = await api.service(argv.service)['find']({
-          name: name
-        })
-        const { _id } = album
-        const result = await api.service(argv.service)[argv.method](_id, {
-          keep: 6 * 60 * 24 * 100 // approximately 100 days of photos
-        })
-        // { name: 'cam1', _id: '5d405c30cafd4e6cb87a3e92' }
-        logger.info(`${argv.service}.${argv.method}`, result)
+    const result = await albumNames.reduce(async (p, name) => {
+      const acc = await p
+      const { total, data } = await api.service(argv.service)['find']({
+        query: { name }
       })
-    )
+      // logger.info(`${argv.service}.find(${name}) = ${total}`, data)
+      const { _id } = data[0]
+      const result = await api.service(argv.service)[argv.method](_id, {
+        keep: 6 * 60 * 24 * 100 // approximately 100 days of photos
+      })
+      // { name: 'cam1', _id: '5d405c30cafd4e6cb87a3e92' }
+      logger.info(`${argv.service}.${argv.method}(${name})`, result)
+      acc.push(result)
+      return acc
+    }, Promise.resolve([]))
+    // logger.info('', result)
   } catch (error) {
     logger.error(error)
   } finally {
